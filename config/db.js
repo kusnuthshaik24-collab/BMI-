14c27f31e6836759485df138c77119b12f7ec1b8
kusnuthshaik24-collab <kusnuth.shaik24@sasi.ac.in>
2026-07-13 15:19:34 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const dns = require("dns");
 
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-const dbURI = 'mongodb+srv://kusnuthshaik24_db_user:kusnuth%4020@cluster0.umvpmlg.mongodb.net/kusnuth?appname=Cluster0'; 
+const dbURI = 'mongodb+srv://kusnuthshaik24_db_user:kusnuth@20@cluster0.umvpmlg.mongodb.net/kusnuth?appname=Cluster0'; 
 
 const connectDB = () => {
   mongoose.connect(dbURI)
