5390a7ad1dce0e56905af7e012d3be47c856436b
kusnuthshaik24-collab <kusnuth.shaik24@sasi.ac.in>
2026-07-13 15:14:24 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
-const dns = require("dns")
-dns.setServers(["8.8.8.8","8.8.4.4"])
-const dbURI = 'mongodb+srv://kusnuthshaik24_db_user:kusnuth@20@cluster0.umvpmlg.mongodb.net/kusnuth?appname=Cluster0'; 
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const dbURI = 'mongodb+srv://kusnuthshaik24_db_user:kusnuth%4020@cluster0.umvpmlg.mongodb.net/kusnuth?appname=Cluster0'; 
 
 const connectDB = () => {
   mongoose.connect(dbURI)
     .then(() => console.log('Connected to MongoDB successfully!'))
-    .catch((err) => console.error('MongoDB connection error:', err));
+    .catch((err) => {
+      console.error('MongoDB connection error:', err);
+      process.exit(1);
+    });
 };
 
 module.exports = connectDB;
