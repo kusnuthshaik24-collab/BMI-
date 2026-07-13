43cde60c907fb59ea2152a5f225bc2458746768b
kusnuthshaik24-collab <kusnuth.shaik24@sasi.ac.in>
2026-07-13 18:54:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const dns = require('dns');
 require('dotenv').config();
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bmi_tracker';
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://kusnuthshaik24_db_user:kusnuth@20@cluster0.umvpmlg.mongodb.net/kusnuth?appname=Cluster0';
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://bmif-mu.vercel.app',
+  credentials: true
+}));
 app.use(express.json());
 
 mongoose.connect(mongoURI)
