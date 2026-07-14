70362835dde677d99858295f5b4bac813d6adb0a
kusnuthshaik24-collab <kusnuth.shaik24@sasi.ac.in>
2026-07-14 14:12:21 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to MongoDB successfully!");
-  } catch (err) {
-    console.error("MongoDB connection error:", err.message);
-    process.exit(1);
-  }
+const dbURI = 'mongodb+srv://badi:Shaik90141@cluster0.umvpmlg.mongodb.net/BMI?retryWrites=true&w=majority'; 
+
+const connectDB = () => {
+  mongoose.connect(dbURI)
+    .then(() => console.log('Connected to MongoDB successfully!'))
+    .catch((err) => console.error('MongoDB connection error:', err));
 };
 
 module.exports = connectDB;
