e8686cd85f854a036c94cde41fa6627fc3c0d7d6
kusnuthshaik24-collab <kusnuth.shaik24@sasi.ac.in>
2026-07-14 06:46:27 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,17 +1,11 @@
 const mongoose = require('mongoose');
-const dns = require("dns");
 
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
-const dbURI = 'mongodb+srv://kusnuthshaik24_db_user:kusnuth@20@cluster0.umvpmlg.mongodb.net/kusnuth?appname=Cluster0'; 
+const dbURI = 'mongodb://localhost:27017/bmi_tracker'; 
 
 const connectDB = () => {
   mongoose.connect(dbURI)
     .then(() => console.log('Connected to MongoDB successfully!'))
-    .catch((err) => {
-      console.error('MongoDB connection error:', err);
-      process.exit(1);
-    });
+    .catch((err) => console.error('MongoDB connection error:', err));
 };
 
 module.exports = connectDB;
